ba4a75174b0aa2b9baf9c9577455c7837c945fb6
Vishwas TA <vishwast@sahaj.ai>
2024-04-02 14:23:23 +0530

    Initial Commit

---
+++ src/main/java/lottery/ticket/App.java
@@ -0,0 +1,12 @@
+package lottery.ticket;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
